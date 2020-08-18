788fca9c53e869b61def4c7540ddb9d2b0dbf288
david lusztig <davidlusztig@outlook.com>
2020-08-18 15:37:31 +0200

    task manager completed! not yet optimized and refactored

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,43 @@
 class TasksController < ApplicationController
-  def all
+  def index
     @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @Task = Task.find(params[:id])
+    @Task.destroy
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
